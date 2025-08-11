041d3d6775339dbbc35e9c158387bed39d6af98e
engrabdurrehman124-lgtm <engrabdurrehman124@gmail.com>
2025-08-11 16:57:11 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -715,5 +715,4 @@ def upload_file():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
