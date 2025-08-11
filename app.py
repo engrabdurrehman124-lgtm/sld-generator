43391697296e2c8d73a38ddd418846731abc2d2d
engrabdurrehman124-lgtm <engrabdurrehman124@gmail.com>
2025-08-11 12:24:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -574,6 +574,7 @@ def view_results():
         return redirect(url_for('index'))
     
     return render_template('results.html', plots=plots, filename=filename)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('Key', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+    
