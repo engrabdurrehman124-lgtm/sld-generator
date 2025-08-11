7e757c392aaff82ceaedb0a3996f8e33d499668a
engrabdurrehman124-lgtm <engrabdurrehman124@gmail.com>
2025-08-11 16:07:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -746,5 +746,5 @@ def upload_file():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('Key', 10000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
